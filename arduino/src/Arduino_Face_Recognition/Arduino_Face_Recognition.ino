b24e4fcf7ce3b04bb1723b3c0ccb65610bec2817
execu <execu@10.0.0.166>
2019-11-14 20:13:01 -0500

    Face Recognition

---
+++ arduino/src/Arduino_Face_Recognition/Arduino_Face_Recognition.ino
@@ -0,0 +1,270 @@
+/********************
+  - Executive Order Corporation - Arduino Tron ESP8266 MQTT Telemetry Transport Machine-to-Machine(M2M)/Internet of Things(IoT)
+  - Arduino Tron Drools-jBPM :: Executive Order Arduino Tron Sensor Processor MQTT AI-IoTBPM Client using AI-IoTBPM Drools-jBPM
+  - Arduino Tron AI-IoTBPM :: Internet of Things Drools-jBPM Expert System using Arduino Tron AI-IoTBPM Processing
+  - Executive Order Corporation
+  - Copyright © 1978, 2019: Executive Order Corporation, All Rights Reserved
+********************/
+
+#include "esp_camera.h"
+#include <WiFi.h>
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT
+//#define CAMERA_MODEL_ESP_EYE
+//#define CAMERA_MODEL_M5STACK_PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE
+#define CAMERA_MODEL_AI_THINKER
+
+#include "camera_pins.h"
+
+// Update these with WiFi network values
+const char* ssid     = "your-ssid"; //  your network SSID (name)
+const char* password = "your-password"; // your network password
+
+WiFiClient client;
+
+// Update these with Arduino Tron service IP address and unique unit id values
+byte server[] = { 10, 0, 0, 2 }; // Set EOSpy server IP address as bytes
+String id = "100666"; // Arduino Tron Device unique unit id
+
+void startCameraServer();
+
+const int httpPort = 5055; // Arduino Tron server is running on default port 5055
+// OpenStreetMap Automated Navigation Directions is a map and navigation app for Android default port 5055
+
+// Values to send in &textMessage= filed
+String textMessage = "text_message";
+
+// Values to send in &keypress= field
+const String TYPE_ALLEVENTS = "allEvents"; // allEvents
+const String TYPE_KEYPRESS_1 = "1.0"; // keypress_1
+const String TYPE_KEYPRESS_2 = "2.0"; // keypress_2
+const String TYPE_REED_RELAY = "4.0"; // reedRelay
+const String TYPE_PROXIMITY = "8.0"; // proximity
+
+// Values to send in &alarm= field
+String alarm_fiels = "ESP32";
+
+// Values to send in &alarm= field
+const String ALARM_GENERAL = "general";
+const String ALARM_SOS = "sos";
+const String ALARM_VIBRATION = "vibration";
+const String ALARM_MOVEMENT = "movement";
+const String ALARM_LOW_SPEED = "lowspeed";
+const String ALARM_OVERSPEED = "overspeed";
+const String ALARM_FALL_DOWN = "fallDown";
+const String ALARM_LOW_POWER = "lowPower";
+const String ALARM_LOW_BATTERY = "lowBattery";
+const String ALARM_FAULT = "fault";
+const String ALARM_POWER_OFF = "powerOff";
+const String ALARM_POWER_ON = "powerOn";
+const String ALARM_DOOR = "door";
+const String ALARM_GEOFENCE = "geofence";
+const String ALARM_GEOFENCE_ENTER = "geofenceEnter";
+const String ALARM_GEOFENCE_EXIT = "geofenceExit";
+const String ALARM_GPS_ANTENNA_CUT = "gpsAntennaCut";
+const String ALARM_ACCIDENT = "accident";
+const String ALARM_TOW = "tow";
+const String ALARM_ACCELERATION = "hardAcceleration";
+const String ALARM_BRAKING = "hardBraking";
+const String ALARM_CORNERING = "hardCornering";
+const String ALARM_FATIGUE_DRIVING = "fatigueDriving";
+const String ALARM_POWER_CUT = "powerCut";
+const String ALARM_POWER_RESTORED = "powerRestored";
+const String ALARM_JAMMING = "jamming";
+const String ALARM_TEMPERATURE = "temperature";
+const String ALARM_PARKING = "parking";
+const String ALARM_SHOCK = "shock";
+const String ALARM_BONNET = "bonnet";
+const String ALARM_FOOT_BRAKE = "footBrake";
+const String ALARM_OIL_LEAK = "oilLeak";
+const String ALARM_TAMPERING = "tampering";
+const String ALARM_REMOVING = "removing";
+
+String ver = "1.03E";
+String irkey = "1.0";
+int loopCounter = 1; // loop counter
+int clientAvail = 0; // client.available() count
+int t_matched_id = -9;
+
+// Set response jBPM Global Variable List
+// kcontext.getKnowledgeRuntime().setGlobal("response", "");
+String response = "";
+String agentCount = "";
+
+void setup(void) {
+  // Arduino IDE Serial Monitor window to emulate what Arduino Tron sensors are reading
+  Serial.begin(115200); // Serial connection from ESP-32 via 3.3v console cable
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  // Connect to WiFi network
+  Serial.println("Executive Order Corporation - Arduino Tron ESP8266 MQTT Telemetry Transport Machine-to-Machine(M2M)/Internet of Things(IoT)");
+  Serial.println("Arduino Tron Drools-jBPM :: Executive Order Arduino Tron Sensor Processor MQTT AI-IoTBPM Client using AI-IoTBPM Drools-jBPM");
+  Serial.println("- Arduino Tron WebCam ver " + ver);
+  Serial.println("Copyright © 1978, 2019: Executive Order Corporation, All Rights Reserved");
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  //initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);//flip it back
+    s->set_brightness(s, 1);//up the blightness just a bit
+    s->set_saturation(s, -2);//lower the saturation
+  }
+  //drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+  // Wait for connection
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(300);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer(); // start camera webserver
+
+  Serial.print("Camera Ready - Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect, the live stream.");
+}
+
+void loop() {
+  if (t_matched_id != -9)  {
+    arduinoTronSend();
+  }
+
+  t_matched_id = -9;
+  delay(1000);
+}
+
+void arduinoTronSend()
+{
+  // Explicitly connect the ESP32 to WiFi-client network
+  if (!client.connect(server, httpPort)) { // http server is running on default port 5055
+    Serial.print("Connection Failed Status: ");
+    Serial.println(WiFi.status());
+    return;
+  }
+
+  Serial.println("Connected");
+  client.print("GET /?id=" + id);
+
+  loopCounter++;
+  agentCount = String(loopCounter);
+
+  client.print("&agentCount=" + agentCount);
+  client.print("&keypress=" + irkey); // keypress=irkey
+
+  client.println(" HTTP/1.1");
+
+  client.println("User-Agent: Arduino Tron ver " + ver);
+  client.println("Content-Length: 0");
+
+  client.println(); // empty line for apache server
+
+  clientAvail = 0;
+  // Wait 1 seconds for server to respond then read response
+  while ((!client.available()) && (clientAvail < 1000)) {
+    delay(1); // wait 1 seconds
+    clientAvail++;
+  }
+
+  response = "";
+  clientAvail = 0;
+  while ((client.available()) && (clientAvail < 5))
+  {
+    String line = client.readStringUntil('\r');
+    if ((line.indexOf("HTTP") == -1) && (clientAvail == 0)) {
+      response = line;
+    }
+    if (line.length() == 1) {
+      clientAvail = 6;
+    }
+    Serial.print(line);
+    clientAvail++;
+  }
+  client.stop();
+
+  Serial.print("Connection Status: ");
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Ok");
+  } else {
+    Serial.print("Error ");
+    Serial.println(WiFi.status());
+  }
+  // WL_NO_SHIELD = 255
+  // WL_IDLE_STATUS = 0
+  // WL_NO_SSID_AVAIL = 1
+  // WL_SCAN_COMPLETED = 2
+  // WL_CONNECTED = 3
+  // WL_CONNECT_FAILED = 4
+  // WL_CONNECTION_LOST = 5
+  // WL_DISCONNECTED = 6
+
+  // WiFi.disconnect(); // DO NOT DISCONNECT WIFI IF YOU WANT TO LOWER YOUR POWER DURING LIGHT_SLEEP_T DELLAY !
+  // wifi_set_sleep_type(LIGHT_SLEEP_T);
+  // digitalWrite(LED2, HIGH); // turn the LED off
+  if (response.length() > 0) {
+    arduinoTronAction();
+  }
+  Serial.println("");
+}
+
+void arduinoTronAction() {
+}
