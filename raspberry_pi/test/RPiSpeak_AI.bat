045b7a242445ee0488aac298f9c5a185594cb33d
execu <execu@10.0.0.166>
2020-01-15 14:18:03 -0500

    Update

---
+++ raspberry_pi/test/RPiSpeak_AI.bat
@@ -0,0 +1,1 @@
+c:\curl\bin\curl -X GET "http://10.0.0.143:8080/say=I_O_T_device_orchestration_gives_computers_the_ability_for_action_in_our_physical_world." -H "accept: application/xml"
---
+++ raspberry_pi/test/RPiSpeak_IOT.bat
@@ -0,0 +1,1 @@
+c:\curl\bin\curl -X GET "http://10.0.0.143:8080/say=I_O_T_creates_opportunities_for_direct_integration_of_computers,_into_our_physical_world,_that_has_never_been_available_before." -H "accept: application/xml"
