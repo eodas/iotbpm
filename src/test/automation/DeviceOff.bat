e9be74ebb54d905f4fa195cb4248fee398bed9d1
execu <execu@10.0.0.174>
2021-04-26 13:38:27 -0400

    Update

--- src/test/automation/DeviceOff.bat
+++ src/test/automation/DeviceOff.bat
@@ -1,1 +1,1 @@
-\curl\bin\curl -X GET "http://localhost:5055/?id=100111&keypress=2.0&textMessage=Device+Off&alarm=none" -H "accept: application/xml"
+\curl\bin\curl -X GET "http://localhost:5055/?id=100111&keypress=2.0&textMessage=Device_is_Off&alarm=none" -H "accept: application/xml"
--- src/test/automation/DeviceOn.bat
+++ src/test/automation/DeviceOn.bat
@@ -1,1 +1,1 @@
-\curl\bin\curl -X GET "http://localhost:5055/?id=100111&keypress=1.0&textMessage=Device+On&alarm=none" -H "accept: application/xml"
+\curl\bin\curl -X GET "http://localhost:5055/?id=100111&keypress=1.0&textMessage=Device_is_On&alarm=none" -H "accept: application/xml"
