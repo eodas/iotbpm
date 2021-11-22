a5f90c36fe7af2c1d9531b26fcb74e352974d5d4
eodas <github@eodas.com>
2021-11-21 19:08:38 -0500

    Update

--- src/main/java/com/iotbpm/bpmrules/IoTBPM.java
+++ src/main/java/com/iotbpm/bpmrules/IoTBPM.java
@@ -230,7 +230,7 @@ public class IoTBPM {
 		// Find public IP address
 		String systemipaddress = "";
 		try {
-			URL url_name = new URL("http://bot.whatismyipaddress.com");
+			URL url_name = new URL("https://myexternalip.com/raw"); // URL("http://bot.whatismyipaddress.com"); No longer providing API
 			BufferedReader sc = new BufferedReader(new InputStreamReader(url_name.openStream()));
 
 			// reads system IPAddress
